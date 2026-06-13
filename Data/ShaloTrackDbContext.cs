b3c7670f30a67e8f5eee468e384b86cf25d38aac
SwenJay <suwencj1@gmail.com>
2026-06-13 08:50:18 +0530

    Testing string connetion to supabase

---
+++ Data/ShaloTrackDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShaloTrack_API.Models;
+
+namespace ShaloTrack_API.Data;
+
+public class ShaloTrackDbContext : DbContext
+{
+    public ShaloTrackDbContext(
+        DbContextOptions<ShaloTrackDbContext> options)
+        : base(options)
+    {
+    }
+    //Tables
+    public DbSet<Customer> Customers => Set<Customer>();
+    public DbSet<Vehicle> Vehicles => Set<Vehicle>();
+    public DbSet<GpsDevice> GpsDevices => Set<GpsDevice>();
+    public DbSet<DeviceAssignment> DeviceAssignments => Set<DeviceAssignment>();
+    public DbSet<DeviceStatus> DeviceStatuses => Set<DeviceStatus>();
+}
